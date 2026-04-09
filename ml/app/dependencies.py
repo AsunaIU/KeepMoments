f3401d7f33847a42ee2455fa4af5691b0cc797e7
Knohhon <dmitriyrezov02@gmail.com>
2026-04-09 18:36:45 +0500

    Dev: added local s3 to config

--- ml/app/dependencies.py
+++ ml/app/dependencies.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.config import Config
 from fastapi import Depends
 
 from app.config import Settings, get_settings
@@ -10,4 +11,6 @@ def get_s3_client(settings: Settings = Depends(get_settings)):
         region_name=settings.AWS_REGION,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        endpoint_url=settings.S3_ENDPOINT_URL,
+        config=Config(signature_version="s3v4"),
     )
